69e048d4c462e207fc21922489a27ebaec728a2c
yasminbraga <32020615+yasminbraga@users.noreply.github.com>
2017-09-16 15:45:32 -0300

    codigo serial

--- serial_py.py
+++ serial_py.py
@@ -1,5 +1,6 @@
 import serial
 comunicacaoSerial = serial.Serial('/dev/ttyACM0', 9600)
-	
+value = comunicacaoSerial.readline()
 while 1:
-	print("corrente: ",comunicacaoSerial.readline()," A")
+	#print("corrente: ",comunicacaoSerial.readline()," A")
+	print (type(value))
