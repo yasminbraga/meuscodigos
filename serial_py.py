afb3f955ca8907950cdd0ca40eaf092160559e2f
yasminbraga <32020615+yasminbraga@users.noreply.github.com>
2017-09-16 16:07:58 -0300

    codigo serial

--- serial_py.py
+++ serial_py.py
@@ -4,9 +4,9 @@ def binario_para_decimal(leitura):
     return str(float(leitura))
 
 comunicacaoSerial = serial.Serial('/dev/ttyACM0', 9600)
-value_bin = comunicacaoSerial.readline()
-value = binario_para_decimal(value_bin)
 
 while 1:
+	value_bin = comunicacaoSerial.readline()
+	value = binario_para_decimal(value_bin)
 	print("corrente: ",value," A")
 	#print ("tipo: ",type(value))
