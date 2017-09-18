8a90b7d875e142f97806f3baec36168d231b22b4
yasminbraga <32020615+yasminbraga@users.noreply.github.com>
2017-09-18 17:45:48 -0300

    checando conexao com a internet

---
+++ checar_conexao.py
@@ -0,0 +1,21 @@
+import socket
+
+confiaveis = ['www.google.com', 'www.yahoo.com', 'www.bb.com.br']
+
+def checar_conexao():
+	global confiaveis
+	for host in confiaveis:
+		connection=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		connection.settimeout(0.5)
+		try:
+			connection_connect=connection.connect_ex((host, 80))
+			if connection_connect==0:
+				return True
+		except:
+			pass
+	connection.close()
+	return False
+
+print(checar_conexao())
+
+
